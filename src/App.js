7607992c29ed34915a0336ae04329b4261fbec8e
Michael Rowlinson <rowlinsonmike@gmail.com>
2020-10-07 14:44:16 -0400

    added styled-components, fonts, background, favicon, global styles

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function App() {
-  return <div className="App">React App</div>;
-}
-
-export default App;
---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyle from "./global-styles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App">React App</div>
+    </>
+  );
+}
+
+export default App;
