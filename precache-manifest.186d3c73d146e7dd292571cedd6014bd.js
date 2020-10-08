5d76d9077264ddc5b9ba69a8e3164e6da95686d7
Michael Rowlinson <rowlinsonmike@gmail.com>
2020-10-08 00:09:13 -0400

    github page

---
+++ precache-manifest.186d3c73d146e7dd292571cedd6014bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2f91a8cd7c31487e21c93c150bceb3",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "636e04fd31b2e4476d93",
+    "url": "/quote-generator/static/js/2.33a19b5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quote-generator/static/js/2.33a19b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457d5f59c723f6ccd2ce",
+    "url": "/quote-generator/static/js/main.858c7018.chunk.js"
+  },
+  {
+    "revision": "5fb379f0906ca3118b63",
+    "url": "/quote-generator/static/js/runtime-main.2f7c151c.js"
+  }
+]);
